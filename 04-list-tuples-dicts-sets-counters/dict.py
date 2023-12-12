bd66421e3af10997e24587634cf3b9b77d339115
Yasya Indra <indrayasya96@gmail.com>
2023-12-12 14:30:19 +0700

    dict

--- 04-list-tuples-dicts-sets-counters/dict.py
+++ 04-list-tuples-dicts-sets-counters/dict.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 empty_dict = {}
 empty_dict2 = dict()
 grades = {"Joel":80, "Tim":95}
@@ -43,3 +45,57 @@ tweet = {
 tweet_keys = tweet.keys()
 tweet_values = tweet.values()
 tweet_items = tweet.items()
+
+for key, value in tweet_items:
+    print(f"Key: {key}, Value: {value}")
+
+
+# print("id" in tweet_keys)
+
+# Default Dict
+# Adalah dictionary kosong yang digunakan sebagai penyimpanan dari data data yang ingin dimasukkan dalam sebuah proses pemograman
+document = ["randomize", "randomly", "randomized", "randomness", "randomization", "randomly", "nonrandom", "pseudorandom", "randomizer", "randomly"]
+
+def count_word(document):
+    words_counts = {}
+    for word in document:
+        if word in words_counts:
+            words_counts[word] += 1
+        else:
+            words_counts[word] = 1
+    return words_counts
+
+print(count_word(document))
+
+
+def count_word_int(document):
+    word_counts = defaultdict(int)
+    for word in document:
+        if word in word_counts:
+            word_counts[word] += 1
+        else:
+            word_counts[word] = 1
+    return word_counts
+
+# print(count_word_plus(document))
+
+def count_word_int(document):
+    word_counts = defaultdict(int)
+    for word in document:
+        if word in word_counts:
+            word_counts[word] += 1
+        else:
+            word_counts[word] = 1
+    return word_counts
+
+dd_list = defaultdict(list)
+dd_dict = defaultdict(dict)
+dd_lambda = defaultdict(lambda: [0,0])
+
+dd_list[2].append(1)
+dd_dict["Joel"]["City"] = "Tangerang Barat"
+dd_lambda[1][1] = 5
+
+# print(dd_list)
+# print(dd_dict)
+# print(dd_lambda)
--- 04-list-tuples-dicts-sets-counters/sets.py
+++ 04-list-tuples-dicts-sets-counters/sets.py
@@ -1,7 +1,12 @@
 primes = {2,3,5,7}
-
 s = set()
 
 s.add(1)
 s.add(2)
 s.add(3)
+
+word_list = ["a", "an", "at", "yet", "you"]
+print("zip" in word_list) # False, but have to check every element
+
+stopwords_set = set(word_list)
+print("zip" in stopwords_set) # very fast to check
