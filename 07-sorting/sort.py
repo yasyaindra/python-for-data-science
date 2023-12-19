e541f398f255b1cb357932a38c39de3160b1fee7
Yasya Indra <indrayasya96@gmail.com>
2023-12-19 09:30:38 +0700

    sorting

--- 07-sorting/sort.py
+++ 07-sorting/sort.py
@@ -1,6 +1,11 @@
 from collections import Counter
+# x = [4, 3, 1, 2]
 
 x = [4, 3, 1, 2, -2, 0, -1]
+
+y = sorted(x, key=abs, reverse=True)
+
+# print(y)
 
 document = ["indra", "indra", "wahyu", "afif", "wahyu","wahyu"]
 
@@ -8,5 +13,5 @@ word_counts = Counter(document)
 
 wc = sorted(word_counts.items(), reverse=True)
 
-print(word_counts.items())
+# print(word_counts.items())
 print(wc)
